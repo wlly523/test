7c351096abd6267a97b144405af0d33e02fcfcac
wanglei <iwanglei1994@163.com>
2018-11-01 18:11:05 +0800

    新增一些东西

---
+++ Module01/src/com/ali/Person1.java
@@ -0,0 +1,24 @@
+package com.ali;
+
+public class Person1 {
+
+    private Integer id;
+    private String name;
+
+    public Person1(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
